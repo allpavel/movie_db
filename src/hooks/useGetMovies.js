3fc33eb72bb780665b9576f0ceb6b6bd8a8d785d
allpavel <allpavel@yandex.ru>
2022-02-09 19:57:18 +0300

    Made searching functionality

--- src/hooks/useGetMovies.js
+++ src/hooks/useGetMovies.js
@@ -12,11 +12,12 @@ export const useGetMovies = () => {
     const [movies, setMovies] = useState(initialState);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
 
-    const getMovies = async (page, searchTerm = '') => {
+    const getMovies = async (searchTerm = '', page) => {
         try {
             setIsLoading(true);
-            const movies = await MovieService.getPopularMovies(page, searchTerm);
+            const movies = await MovieService.getPopularMovies(searchTerm, page);
             setMovies(prev => ({
                 ...movies,
                 results:
@@ -30,8 +31,9 @@ export const useGetMovies = () => {
     }
 
     useEffect(() => {
-        getMovies(1);
-    }, []);
+        setMovies(initialState)
+        getMovies(searchTerm, 1);
+    }, [searchTerm]);
 
-    return { movies, isLoading, error };
+    return { movies, isLoading, error, searchTerm, setSearchTerm };
 }
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,22 +1,41 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import { IMAGE_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from '../API/config';
 import { HeroImage } from '../components/HeroImage/HeroImage';
+import { Grid } from '../components/Grid/Grid';
+import { Thumbnail } from '../components/Thumbnail/Thumbnail';
+import { SearchBar } from '../components/SearchBar/SearchBar';
 import { useGetMovies } from '../hooks/useGetMovies';
 
 
 export const Movies = () => {
-    const { movies, isLoading, error } = useGetMovies();
+    const { movies, isLoading, error, searchTerm, setSearchTerm } = useGetMovies();
 
     return (
         <>  
-            {movies.results[0]
+            {!searchTerm && movies.results[0]
                 ? <HeroImage
                     image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${movies.results[0].backdrop_path}`}
                     title={movies.results[0].original_title}
                     description={movies.results[0].overview}
-                />
-                : <h1>Nothing to show</h1>}
-            <h1>Home Page</h1>
+                    />
+                : ''}
+            <SearchBar setSearchTerm={setSearchTerm} />
+            <Grid header={searchTerm ? 'Search Result' : 'Popular Movies'}>
+                {movies.results.map(movie => (
+                    <div key={uuidv4()}>
+                        <Thumbnail
+                            clickable
+                            image={
+                                movie.poster_path
+                                    ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                                    : 'No image'
+                            }
+                            movieId={movie.id}
+                        />
+                    </div>
+                ))}
+            </Grid>
         </>        
     )
 };
